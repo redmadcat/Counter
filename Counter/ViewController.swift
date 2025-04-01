53e92a594d38ae18bf0630bb7d782de2a0885eb2
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-04-01 16:24:20 +0300

    fix: func private modifiers/formatter implementation
    Fix func private/class final modifiers, formatter implementation, based on code review.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,22 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    @IBOutlet weak var textViewLog: UITextView!
-    @IBOutlet weak var labelCounter: UILabel!
-    @IBOutlet weak var buttonReset: UIButton!
-    @IBOutlet weak var buttonDecrease: UIButton!
-    @IBOutlet weak var buttonIncrease: UIButton!
+final class ViewController: UIViewController {
+    @IBOutlet weak private var textViewLog: UITextView!
+    @IBOutlet weak private var labelCounter: UILabel!
+    @IBOutlet weak private var buttonReset: UIButton!
+    @IBOutlet weak private var buttonDecrease: UIButton!
+    @IBOutlet weak private var buttonIncrease: UIButton!
     
     private let counterDescription: String = "Counter value: "
-    private let dateFormatter: DateFormatter = DateFormatter()
     private let counterStep: Int = 1
     private var counterValue: Int = 0
-        
+    
+    private let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ru_Ru")
+        formatter.dateFormat = "dd.MM.YYYY HH:mm:ss"
+        return formatter
+    }()
+      
     override func viewDidLoad() {
         super.viewDidLoad()
-        dateFormatter.locale = Locale(identifier: "ru_Ru")
-        dateFormatter.dateFormat = "dd.MM.YYYY HH:mm:ss"
     }
     
     private func addLogMessage(message: String) {
@@ -34,7 +38,7 @@ class ViewController: UIViewController {
         addLogMessage(message: "Value changed to: \(counterValue)")
     }
 
-    @IBAction func buttonIncreaseDidTap(_ sender: Any) {
+    @IBAction private func buttonIncreaseDidTap(_ sender: Any) {
         if (counterValue == Int.max) {
             addLogMessage(message: "Max value has been reached")
         } else {
@@ -43,7 +47,7 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func buttonDecreaseDidTap(_ sender: Any) {
+    @IBAction private func buttonDecreaseDidTap(_ sender: Any) {
         if (counterValue - counterStep) >= 0 {
             counterValue -= counterStep
             updateCounterDescription()
@@ -52,7 +56,7 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func buttonResetDidTap(_ sender: Any) {
+    @IBAction private func buttonResetDidTap(_ sender: Any) {
         if (counterValue == 0) {
             addLogMessage(message: "Value already reset")
         } else {
